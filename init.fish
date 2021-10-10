141dafcfa5fbdd2e8f9d3b74c5b337ec1ea1ea47
Allen Hunn <allenhunn@gmail.com>
2021-10-10 04:18:20 -0400

    added packages

--- init.fish
+++ init.fish
@@ -5,6 +5,9 @@ sudo aura -S npm --noconfirm
 sudo aura -S pip --noconfirm
 sudo aura -S speedtest-cli --noconfirm
 sudo aura -S vim --noconfirm
+sudo aura -S anaconda --noconfirm
+git config --global user.name "Allen Hunn"
+git config --global user.email allenhunn@gmail.com
 curl -sLf https://spacevim.org/install.sh | bash
 vim
 vim
