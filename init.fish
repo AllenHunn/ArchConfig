1dee0a66f9bbaa7d83826319909c03bf51f8a201
Allen Hunn <allenhunn@gmail.com>
2021-10-10 03:41:11 -0400

    initial

---
+++ init.fish
@@ -0,0 +1,12 @@
+sudo pacman -Syu --noconfirm
+sudo pacman -S aura --noconfirm
+sudo aura -S nodejs --noconfirm
+sudo aura -S npm --noconfirm
+sudo aura -S pip --noconfirm
+sudo aura -S speedtest-cli --noconfirm
+sudo aura -S vim --noconfirm
+curl -sLf https://spacevim.org/install.sh | bash
+vim
+vim
+cp -RT ./.config ~/.config
+cp -RT ./.SpaceVim.d ~/.SpaceVim.d
