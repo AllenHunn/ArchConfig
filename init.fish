7e8818a4ea65b13ad95c05eca13ab45ad435d6b9
Allen Hunn <allenhunn@gmail.com>
2021-10-10 22:42:48 -0400

    Added more languages

--- init.fish
+++ init.fish
@@ -1,5 +1,6 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -S aura --noconfirm
+sudo aura -Syu --noconfirm
 sudo aura -S nodejs --noconfirm
 sudo aura -S npm --noconfirm
 sudo aura -S pip --noconfirm
@@ -7,6 +8,9 @@ sudo aura -S speedtest-cli --noconfirm
 sudo aura -S vim --noconfirm
 sudo aura -A nomachine --noconfirm
 sudo aura -S rust --noconfirm
+sudo aura -S go --noconfirm
+sudo aura -S r --noconfirm
+sudo aura -S julia --noconfirm
 sudo aura -S anaconda --noconfirm
 git config --global user.name "Allen Hunn"
 git config --global user.email allenhunn@gmail.com
