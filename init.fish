5d75df22a09b56c80539befa87cb3b3bd72a5327
AllenHunn <allenhunn@gmail.com>
2021-10-12 01:08:14 -0400

    Update init.fish

--- init.fish
+++ init.fish
@@ -1,22 +1,24 @@
+#! /usr/bin/fish
+
 sudo pacman -Syu --noconfirm
-sudo pacman -S aura --noconfirm
-sudo aura -Syu --noconfirm
-sudo aura -S nodejs --noconfirm
-sudo aura -S npm --noconfirm
-sudo aura -S pip --noconfirm
-sudo aura -S speedtest-cli --noconfirm
-sudo aura -S vim --noconfirm
-sudo aura -A nomachine --noconfirm
-sudo aura -S rust --noconfirm
-sudo aura -S go --noconfirm
-sudo aura -S r --noconfirm
-sudo aura -S julia --noconfirm
-sudo aura -S anaconda --noconfirm
+sudo pacman -S aura --noconfirm --needed
+sudo aura -Syu --noconfirm --needed
+sudo aura -S nodejs --noconfirm --needed
+sudo aura -S npm --noconfirm --needed
+sudo aura -S pip --noconfirm --needed
+sudo aura -S speedtest-cli --noconfirm --needed
+sudo aura -S vim --noconfirm --needed
+sudo aura -A nomachine --noconfirm --needed
+sudo aura -S rust --noconfirm --needed
+sudo aura -S go --noconfirm --needed
+sudo aura -S r --noconfirm --needed
+sudo aura -S julia --noconfirm --needed
+sudo aura -S anaconda --noconfirm --needed
 git config --global user.name "Allen Hunn"
 git config --global user.email allenhunn@gmail.com
 curl -sLf https://spacevim.org/install.sh | bash
 vim
 vim
-cp -RT ./.config ~/.config
+sudo cp -RT ./.config ~/.config
 cp -RT ./.SpaceVim.d ~/.SpaceVim.d
 cargo install deno --locked
